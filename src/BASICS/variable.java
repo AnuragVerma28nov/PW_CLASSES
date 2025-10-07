8c61331de9a81286f8a186543f6678a4861ea681
AnuragVerma28nov <anuragverma28nov@gmail.com>
2025-10-07 23:52:38 +0530

    file added

---
+++ src/BASICS/variable.java
@@ -0,0 +1,11 @@
+package BASICS;
+
+public class variable {
+    public static void main(String[] args) {
+        int x; //Declaration
+        x = 6; //Initialisation
+        int y = 8;//Declaration and Initialisation
+        System.out.println(x);
+        System.out.println(y);
+    }
+}
