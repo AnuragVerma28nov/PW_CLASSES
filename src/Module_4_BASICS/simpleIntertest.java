e3d9ea46e050aeef316213f42eea5b614f1873dd
AnuragVerma28nov <anuragverma28nov@gmail.com>
2025-10-09 20:06:24 +0530

    file added

---
+++ src/Module_4_BASICS/simpleIntertest.java
@@ -0,0 +1,16 @@
+package Module_4_BASICS;
+
+public class simpleIntertest {
+    public static void main(String[] args) {
+        double p = 1500;
+        double r = 1.5;
+        double t = 6;
+
+        double si = (p * r * t) / 100;
+        System.out.println("Simple Interest: " +si);
+    }
+
+}
+
+//Output :
+//Simple Interest: 135.0
