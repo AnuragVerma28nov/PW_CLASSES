a60f85f9a5bda1db832adc4cc58db85bfc5f5557
AnuragVerma28nov <anuragverma28nov@gmail.com>
2025-10-12 00:34:34 +0530

    operators added

---
+++ src/Module_4_BASICS/bitwiseOperators.java
@@ -0,0 +1,13 @@
+package Module_4_BASICS;
+
+public class bitwiseOperators {
+    public static void main(String[] args) {
+        // initialize p
+        int p = 5;
+        System.out.println(p<<2);
+        // Shifting the value of p towards the left two positions
+    }
+}
+
+//Output
+//20
